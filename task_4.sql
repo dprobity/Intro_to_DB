aeba2cebf408c6ce183d7a6174825c975e58e9f2
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-02-09 06:54:22 -0600

    Pushed stuffs

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,10 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Retrieve the full description of the Books table from INFORMATION_SCHEMA
-SELECT TABLE_NAME, COLUMN_NAME, COLUMN_DEFAULT, IS_NULLABLE, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
+-- Retrieve the column description for the Books table from INFORMATION_SCHEMA
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'Books' 
+WHERE TABLE_NAME = 'Books'
   AND TABLE_SCHEMA = 'alx_book_store';
+
+
